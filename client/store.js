6eda88498eaa06567bd6d7cec9d31b6ae10048a1
Eric Sproles <tron04@gmail.com>
2018-04-02 15:29:59 -0700

    first commit

---
+++ client/store.js
@@ -0,0 +1,12 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+
+// import the root reducers
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for the default data
