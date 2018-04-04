ad0b97b908d494bd51f095c77d1af57958beb867
Eric Sproles <tron04@gmail.com>
2018-04-03 17:10:52 -0700

    pushing through vscode

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,11 @@ const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        {this.props.posts.map((post, index) => <Photo {...this.props}>)}
+        {this.props.posts.map((post, index) =>
+        <Photo {...this.props}
+        key={index}
+        post={post}
+        />)}
       </div>
     )
   }
